f8dd0426d3cffdc4d4edb4520ae5dc16b335b5d4
Liam <lms.seeney@gmail.com>
2022-06-07 09:17:09 +0100

    Adds Solution
    -ASP.NET MVC project added

---
+++ JobOrganiserWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JobOrganiserWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
